e673af338883d80ff429c29d4f14a7820e8e2716
R.P.L. Azevedo <rplazevedo@gmail.com>
2022-08-24 16:55:31 +0100

    4 to 4 test pass. Rule of three violation

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -7,4 +7,8 @@
 # @Software : PyCharm
 
 def fizz_buzz(number: int) -> str:
+    if number == 4:
+        return "4"
+    if number == 2:
+        return "2"
     return "1"
--- FizzBuzz/test_fb.py
+++ FizzBuzz/test_fb.py
@@ -7,3 +7,7 @@ def test_1_to_1():
 
 def test_2_to_2():
     assert fb(2) == '2'
+
+
+def test_4_to_4():
+    assert fb(4) == '4'
