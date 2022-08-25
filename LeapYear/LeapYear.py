afb314da4422f336511b7a39e0ea849d6ab7cf84
R.P.L. Azevedo <rplazevedo@gmail.com>
2022-08-25 15:33:31 +0100

    Added LeapYear

---
+++ LeapYear/LeapYear.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Project  : test_fb.py
+# @File     : LeapYear
+# @Date     : 2022-08-25T15:25:50
+# @Author   : R.P.L. Azevedo
+# @Email    : rplazevedo@gmail.com
+# @Software : PyCharm
+
