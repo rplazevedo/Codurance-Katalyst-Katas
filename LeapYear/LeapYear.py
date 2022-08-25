3cabd8bf9b3698d64fa90d0761b349fc9b9e9c76
R.P.L. Azevedo <rplazevedo@gmail.com>
2022-08-25 16:46:35 +0100

    Finished LeapYear

--- LeapYear/LeapYear.py
+++ LeapYear/LeapYear.py
@@ -6,3 +6,24 @@
 # @Email    : rplazevedo@gmail.com
 # @Software : PyCharm
 
+class Year:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def is_leapyear(year: int) -> bool:
+        return Year.div_by_400(year) or (
+                Year.div_by_4(year) and Year.not_div_by_100(year))
+
+    @staticmethod
+    def not_div_by_100(year: int) -> bool:
+        return not year % 100 == 0
+
+    @staticmethod
+    def div_by_4(year: int) -> bool:
+        return year % 4 == 0
+
+    @staticmethod
+    def div_by_400(year: int) -> bool:
+        return year % 400 == 0
+
