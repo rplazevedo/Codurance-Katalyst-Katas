a2448bb1865ee0a51e76aded806c7e18bbcb24fd
R.P.L. Azevedo <rplazevedo@gmail.com>
2022-08-31 14:22:42 +0100

    Refactored tests to use fixture for class creation and teardown

--- MarsRover/test_mars_rover.py
+++ MarsRover/test_mars_rover.py
@@ -6,56 +6,62 @@
 # @Email    : rplazevedo@gmail.com
 # @Software : PyCharm
 
+import pytest
+
 from MarsRover import MarsPlateau, MarsRover
 
 
-def test_can_create_grid():
-    Plateau = MarsPlateau()
-    assert isinstance(Plateau, MarsPlateau)
+@pytest.fixture()
+def plateau():
+    # print("setup")
+    yield MarsPlateau()
+    # print("teardown")
 
 
-def test_grid_is_10_by_10():
-    Plateau = MarsPlateau()
-    assert Plateau._grid.shape == (10, 10)
+@pytest.fixture()
+def rover():
+    # print("setup")
+    yield MarsRover()
+    # print("teardown")
 
 
-def test_add_obstacle_at_2_2():
-    Plateau = MarsPlateau()
-    Plateau.add_obstacle((2, 2))
-    assert Plateau._grid[2, 2] == 1
+def test_can_create_grid(plateau):
+    # Plateau = MarsPlateau()
+    assert isinstance(plateau, MarsPlateau)
 
 
-def test_add_multiple_obstacles():
-    Plateau = MarsPlateau()
-    Plateau.add_mutiple_obstacles([(1, 2), (2, 3)])
-    assert Plateau._grid[1, 2] == 1
-    assert Plateau._grid[2, 3] == 1
-    assert Plateau._grid[2, 2] == 0
+def test_grid_is_10_by_10(plateau):
+    assert plateau._grid.shape == (10, 10)
 
 
-def test_is_obstacle_at_coord():
-    Plateau = MarsPlateau()
-    Plateau.add_obstacle((2, 2))
-    Plateau.is_obstacle_at_coord((2, 2))
-    assert Plateau.is_obstacle_at_coord((2, 2))
+def test_add_obstacle_at_2_2(plateau):
+    plateau.add_obstacle((2, 2))
+    assert plateau._grid[2, 2] == 1
 
 
-def test_rover_move_up_obs_at_0_1():
-    Plateau = MarsPlateau()
-    Rover = MarsRover()
-    Plateau.add_obstacle((0, 1))
-    assert Rover.exe("M", Plateau) == "O:0:0:N"
+def test_add_multiple_obstacles(plateau):
+    plateau.add_mutiple_obstacles([(1, 2), (2, 3)])
+    assert plateau._grid[1, 2] == 1
+    assert plateau._grid[2, 3] == 1
+    assert plateau._grid[2, 2] == 0
 
 
-def test_rover_move_up_obs_at_0_3():
-    Plateau = MarsPlateau()
-    Rover = MarsRover()
-    Plateau.add_obstacle((0, 3))
-    assert Rover.exe("MMMM", Plateau) == "O:0:2:N"
+def test_is_obstacle_at_coord(plateau):
+    plateau.add_obstacle((2, 2))
+    plateau.is_obstacle_at_coord((2, 2))
+    assert plateau.is_obstacle_at_coord((2, 2))
 
 
-def test_rover_move_up_obs_at_3_3():
-    Plateau = MarsPlateau()
-    Rover = MarsRover()
-    Plateau.add_obstacle((3, 3))
-    assert Rover.exe("RMMMLMMM", Plateau) == "O:3:2:N"
+def test_rover_move_up_obs_at_0_1(plateau, rover):
+    plateau.add_obstacle((0, 1))
+    assert rover.exe("M", plateau) == "O:0:0:N"
+
+
+def test_rover_move_up_obs_at_0_3(plateau, rover):
+    plateau.add_obstacle((0, 3))
+    assert rover.exe("MMMM", plateau) == "O:0:2:N"
+
+
+def test_rover_move_up_obs_at_3_3(plateau, rover):
+    plateau.add_obstacle((3, 3))
+    assert rover.exe("RMMMLMMM", plateau) == "O:3:2:N"
